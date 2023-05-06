3bdbc652ee1b06890e3191f57e7374317d959a37
Simon Wunderlin <swunderlin@gmail.com>
2023-05-06 08:19:36 +0200

    added listen timeout poc

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ This project aims to explore how `java.nio.channels` socket timeout can be confi
 ```bash
 mvn clean compile exec:java
 ```
+
+## references
+
+- [java.nio.channels.SocketChannel](https://docs.oracle.com/javase/8/docs/api/java/nio/channels/SocketChannel.html)
+- [Networking in Java: non-blocking NIO, blocking NIO and IO](https://technfun.wordpress.com/2009/01/29/networking-in-java-non-blocking-nio-blocking-nio-and-io/)
+- [Java NIO: Socket Channels](http://tutorials.jenkov.com/java-nio/socket-channel.html)
+- https://stackoverflow.com/questions/2866557/timeout-for-socketchannel-doesnt-work 
--- src/main/java/net/wunderlin/test/Main.java
+++ src/main/java/net/wunderlin/test/Main.java
@@ -1,31 +1,144 @@
 package net.wunderlin.test;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.slf4j.Logger;
 
 public class Main {
 
-    // create nio socket channel
-    public static SocketChannel create_channel(String host, int port, int timeout) throws IOException {
-        // instantiate a channel
-        SocketChannel channel = SocketChannel.open();
-        // set socket timeout
-        channel.socket().setSoTimeout(timeout);
-        // connect to the server
-        channel.connect(new InetSocketAddress(host, port));
-        // set the channel to non-blocking
-        channel.configureBlocking(false);
-        // return the channel
-        return channel;
+    // instantiate static logger
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(Main.class);
+
+    /**
+     * main method
+     * @param args
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) {
+
+        /**
+         * This example shows how to handle listen timeouts with java.nio.Channel
+         * when non-blocking connections are used.
+         */
+        int listen_timeout = (int) 30_000;
+        try {
+            channel_listen_timeout(listen_timeout);
+        } catch (SocketTimeoutException e) {
+            logger.error("timeout");
+            return;
+        } catch (IOException e) {
+            logger.error("error", e);
+        }
+
     }
 
-    // main method
-    public static void main(String[] args) throws IOException {
-        System.out.println("connecting to localhost:9999, timeout 30 seconds");
+    private static void channel_listen_timeout(int listen_timeout) throws IOException, SocketTimeoutException {
+        logger.info("Listening on localhost: 9999, timeout {} seconds", (int) (listen_timeout / 1000));
+        
+        // prepare listener
+        Selector selector = Selector.open();
+        ServerSocketChannel serverSocket = ServerSocketChannel.open();
+        serverSocket.bind(new InetSocketAddress("localhost", 9999));
 
-        // create a channel
-        SocketChannel channel = create_channel("localhost", 9999, 30000);
+        // serverSocket.socket().setSoTimeout(listen_timeout); // -> does not work
+        serverSocket.configureBlocking(false);
 
+        // register accept 
+        int ops = serverSocket.validOps();
+        serverSocket.register(selector, ops);
+        ByteBuffer buffer = ByteBuffer.allocate(256);
+
+        logger.info("ready to accept connection (timeout {}) ...", (int) (listen_timeout / 1000));
+
+        // start a thread and sleep for 2 seconds
+        new Thread(() -> {
+            logger.info("sleeping {} seconds", (int) ((listen_timeout + 2_000) / 1000));
+            try {
+                Thread.sleep(listen_timeout + 2_000);
+            } catch (InterruptedException e) {
+                logger.error("error sleeping", e);
+            }
+            logger.info("waking up");
+        }).start();
+        
+        // current time
+        long start = System.currentTimeMillis();
+
+        // wait for selector to be fired, if not within timeout (start + listen_timeout)
+        // then throw a timeout exception
+        SelectionKey key = null;
+        while (true) {
+
+            // check if we have reached the timeout
+            if (System.currentTimeMillis() > start + listen_timeout) {
+                serverSocket.close();
+                throw new SocketTimeoutException();
+            }
+
+            //logger.info("selecting ...");
+            if (selector.selectNow() <= 0) continue; // non-blocking
+            logger.info("processing selection ...");
+            
+            // we got one or more event(s)
+            // refresh timeout and handle it
+            start = System.currentTimeMillis();
+
+            // handle events
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iter = selectedKeys.iterator();
+            while (iter.hasNext()) {
+
+                key = iter.next();
+
+                if (key.isAcceptable()) {
+                    register(selector, serverSocket);
+                }
+
+                if (key.isReadable()) {
+                    answerWithEcho(buffer, key);
+                }
+                iter.remove();
+            }
+
+            logger.info("next round ...");
+        }
     }
+
+    private static void register(Selector selector, ServerSocketChannel serverSocket)
+      throws IOException {
+ 
+        SocketChannel client = serverSocket.accept();
+        client.configureBlocking(false);
+        client.register(selector, SelectionKey.OP_READ | SelectionKey.OP_CONNECT);
+    }
+
+    private static void answerWithEcho(ByteBuffer buffer, SelectionKey key)
+    throws IOException {
+
+      SocketChannel client = (SocketChannel) key.channel();
+      int r = client.read(buffer);
+      if (r == -1 || new String(buffer.array()).trim().equals("exit")) {
+          client.close();
+          System.out.println("Not accepting client messages anymore");
+      }
+      else {
+          buffer.flip();
+          client.write(buffer);
+          buffer.clear();
+      }
+  }
 }
